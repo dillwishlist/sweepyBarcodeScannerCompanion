6ac803eccd8eb521fbdfda4a03c828fd343a606d
ataylor-orcsd-org <ataylor-orcsd-org@users.noreply.github.com>
2021-01-22 12:44:26 -0500

    Tweaks

--- webroot/index.php
+++ webroot/index.php
@@ -127,56 +127,13 @@ elseif ($barcodeValue)
             default:
         }
         $assetID = $previousAssetID;
-//        $html = $html . "<script type=\"text/javascript\">startFadeDec(50, 255, 50, 255, 255, 255, 20);</script>\n";
         if ($assetID)
         {
             $assetType = GetAssetType($assetID,$cfg['baseDomain']);
-            if ($assetType == 901 || $assetType == 908)
-            {
-                if ($assetID == $assetRelationParentID)
-                {
-                    $html = $html . "<h1>Container Unset</h1>\n";
-                    EchoAssetLinks($baseLSURL,$assetID);
-                    $addContainerAssetRelation = false;
-                    $assetRelationParentID = '';
-    
-                }
-                else
-                {
-                    $html = $html . "<h1>Container Set</h1>\n";
-                    SetAssetCustomBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    InsertAssetCommentBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    EchoAssetLinks($baseLSURL,$assetID);
-                    $addContainerAssetRelation = true;
-                    $assetRelationParentID = $assetID;
-                }
-            }
-            else
-            {
-                if ($addContainerAssetRelation)
-                {
-                    $html = $html . "<h1>Adding Asset Relationship</h1>\n";
-                    SetAssetCustomBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    InsertAssetCommentBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    EchoAssetLinks($baseLSURL,$assetID);
-                    AddAssetRelationToParent($assetID,$assetRelationParentID,$cfg['baseDomain'],$containerAssetRelationshipType);
-                }
-                else
-                {
-                    if ($inventoryRoom)
-                        {
-                            $oldInventoryInfo = GetAssetInventoryInfo($assetID,$cfg['baseDomain']);
-                            PrintAssetInventoryInfo($cfg['baseDomain'],$oldInventoryInfo);
-                            InsertAssetCommentAuditTrail($assetID,$cfg['baseDomain'],$oldInventoryInfo,false);
-                            UpdateAssetInventory($assetID,$inventoryLocation,$inventoryBuilding,$inventoryDepartment,$inventoryBranchoffice,$cfg['baseDomain']);
-                        }
-                    SetAssetCustomBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    InsertAssetCommentBarcodeScanTime($assetID,$cfg['baseDomain']);
-                    PrintAssetInventoryInfo($cfg['baseDomain'],GetAssetInventoryInfo($assetID,$cfg['baseDomain']),$assetID);
-                    PrintAssetUserRelations(GetAssetUserRelations($assetID,$cfg['baseDomain']));
-                    PrintUserTickets(array_unique(GetAssetTickets($assetID,$cfg['baseDomain'])));
-                }
-            }
+
+            PrintAssetInventoryInfo($cfg['baseDomain'],GetAssetInventoryInfo($assetID,$cfg['baseDomain']),$assetID);
+            PrintAssetUserRelations(GetAssetUserRelations($assetID,$cfg['baseDomain']));
+            PrintUserTickets(array_unique(GetAssetTickets($assetID,$cfg['baseDomain'])));
 
     }
     else
